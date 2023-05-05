a2032c65ac586e1fea4f5ce5329f0f08b02fb412
Umut Kuzu <120611698+arenumutcesmeci@users.noreply.github.com>
2023-05-05 12:58:35 +0300

    Add files via upload

---
+++ patikaDongulerWhileForeach/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace donguler_while_foreach;
+class Program
+{
+    static void Main(string[] args)
+    {
+        // While
+        // 1den başlayarak konsoldan girilen sayıya kadar (sayı dahil) ortalama hesaplama
+
+        Console.Write("Lütfen Bir Sayı Giriniz:");
+        int sayi = int.Parse(Console.ReadLine());
+        int sayac = 1;
+        int toplam = 0;
+
+        while (sayac <= sayi)
+        {
+            toplam+= sayac;
+            sayac ++;
+        }
+        Console.WriteLine(toplam/sayi);
+
+        // a'dan z'ye kadar tüm harfleri console yazdır
+        char character = 'a';
+        while (character <= 'z')
+        {
+            Console.Write(character);
+            character ++;
+        }
+
+        //Foreach
+        Console.WriteLine("******Foreach******");
+        
+
+        string[] arabalar = {"BMW", "Ford", "Toyata", "Nissan"};
+
+        foreach (var araba in arabalar)
+        {
+            Console.WriteLine(araba);
+        }
+
+    }
+}
