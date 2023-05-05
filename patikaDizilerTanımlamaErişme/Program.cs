0665bdbf952de67165627760827bea12f663eb9f
Umut Kuzu <120611698+arenumutcesmeci@users.noreply.github.com>
2023-05-05 14:52:08 +0300

    Add files via upload

---
+++ patikaDizilerTanımlamaErişme/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace diziler;
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Dizi Tanımlama
+        string[] renkler = new string[5];
+
+        string[] hayvanlar = {"Kedi", "Köpek", "Kuş", "Maymun"};
+
+        int[] dizi;
+        dizi = new int[5];
+
+        // Diziler Değer Atama ve Erişim
+        renkler[0] = "Mavi";
+        dizi[3] = 10;
+
+         Console.WriteLine(hayvanlar[1]);
+        Console.WriteLine(dizi[3]);
+        Console.WriteLine(renkler[0]);
+        
+
+        //Döngüler ile dizi kullanımı
+
+        //Klavyeden girilen n tane sayının ortalamasını alan program
+
+        Console.Write("Lütfen dizinin eleman sayisini giriniz:");
+        int diziUzuluğu = int.Parse(Console.ReadLine());
+        int[] sayiDizisi = new int[diziUzuluğu];
+
+        for (int i = 0; i < diziUzuluğu; i++)
+        {
+            Console.Write("Lütfen {0}. sayiyi giriniz:" , i+1);
+            sayiDizisi[i] = int.Parse(Console.ReadLine());
+        }
+
+        int toplam = 0;
+        foreach (var sayi in sayiDizisi)
+            toplam += sayi;
+
+        Console.WriteLine("Ortalama :"+ toplam/diziUzuluğu);
+    }
+}
